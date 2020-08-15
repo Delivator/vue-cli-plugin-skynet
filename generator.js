09983cbc69b243eb32e464ebc413da697c572ecc
Delivator <info@delivator.me>
2020-08-15 18:06:35 +0200

    Changed generator

--- generator.js
+++ generator.js
@@ -1,6 +1,5 @@
 const fs = require("fs")
 
-
 module.exports = (api) => {
   api.extendPackage({
     dependencies: {
@@ -11,9 +10,8 @@ module.exports = (api) => {
   api.onCreateComplete(() => {
     // adapted from https://github.com/vuetifyjs/vue-cli-plugins/blob/7ffbd824ae1d4731a7e70a65a1db52f7a643e4b6/packages/vue-cli-plugin-vuetify/generator/tools/vuetify.js#L61-79
     const config = api.resolve("vue.config.js")
-    const deploy_script = api.resolve("deploy_skynet.js")
 
-    fs.copyFileSync("./deploy_skynet.js", deploy_script)
+    api.render({"./deploy_skynet.js": "./deploy_skynet.js"})
 
     if (!fs.existsSync(config)) {
       fs.writeFileSync(config, "module.exports = {}", "utf8")
