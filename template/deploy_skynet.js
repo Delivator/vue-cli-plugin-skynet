6139df4cb4575874ae8f4f219cd5bc5f984adb4d
Delivator <info@delivator.me>
2020-08-20 19:35:58 +0200

    Removed semicolons

--- template/deploy_skynet.js
+++ template/deploy_skynet.js
@@ -10,13 +10,13 @@ opts.portalUrl = config.portal
 console.log(`Uploading ${path} to ${config.portal}`)
 
 function updateNamebaseDomain(skylink) {
-  if (!config.namebaseDomain) return console.error("config.namebaseDomain cannot be empty");
-  if (!config.namebaseAPIKey) return console.error("config.namebaseAPIKey cannot be empty");
-  if (!config.namebaseAPISecret) return console.error("config.namebaseAPISecret cannot be empty");
+  if (!config.namebaseDomain) return console.error("config.namebaseDomain cannot be empty")
+  if (!config.namebaseAPIKey) return console.error("config.namebaseAPIKey cannot be empty")
+  if (!config.namebaseAPISecret) return console.error("config.namebaseAPISecret cannot be empty")
 
-  const credentials = Buffer.from(`${config.namebaseAPIKey}:${config.namebaseAPISecret}`);
-  const encodedCredentials = credentials.toString("base64");
-  const authorization = `Basic ${encodedCredentials}`;
+  const credentials = Buffer.from(`${config.namebaseAPIKey}:${config.namebaseAPISecret}`)
+  const encodedCredentials = credentials.toString("base64")
+  const authorization = `Basic ${encodedCredentials}`
 
   const jsonBody = {
     records: [
@@ -27,7 +27,7 @@ function updateNamebaseDomain(skylink) {
         ttl: 0
       }
     ]
-  };
+  }
 
   const requestOptions = {
     method: "PUT",
@@ -37,26 +37,26 @@ function updateNamebaseDomain(skylink) {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(jsonBody)
-  };
+  }
 
   fetch(`https://namebase.io/api/v0/dns/domains/${config.namebaseDomain}`, requestOptions)
     .then(response => {
-      console.log(response);
-      return response.json();
+      console.log(response)
+      return response.json()
     })
     .then(json => {
       if (!json || !json.success) {
-        console.error("Namebase error:");
-        console.error(json);
-        return;
+        console.error("Namebase error:")
+        console.error(json)
+        return
       } else {
-        console.log(`Domain "${config.namebaseDomain}" successfully updated`);
-        console.log(`TX Hash: ${json.txHash}`);
-        console.log(`rawNameState: ${json.rawNameState}`);
+        console.log(`Domain "${config.namebaseDomain}" successfully updated`)
+        console.log(`TX Hash: ${json.txHash}`)
+        console.log(`rawNameState: ${json.rawNameState}`)
       }
     })
     .catch(error => {
-      console.error(error);
+      console.error(error)
     })
 }
 
@@ -65,6 +65,6 @@ skynet
   .then((resp) => {
     const skylink = resp.replace("sia://", "")
     console.log(`Done: ${config.portal}/${skylink}/`)
-    if (config.enableNamebase) updateNamebaseDomain(skylink);
+    if (config.enableNamebase) updateNamebaseDomain(skylink)
   })
   .catch((err) => console.error(err.message))
